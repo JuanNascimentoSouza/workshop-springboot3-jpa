b46f974661cc7f4d11db74b7249213dc1001652b
Juan <juan.nascimento.999@gmail.com>
2022-11-23 20:56:45 -0300

    maven, jpa, springboot

---
+++ juanns/src/main/java/com/juannascimento/juanns/JuannsApplication.java
@@ -0,0 +1,13 @@
+package com.juannascimento.juanns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuannsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuannsApplication.class, args);
+	}
+
+}
---
+++ juanns/src/test/java/com/juannascimento/juanns/JuannsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juannascimento.juanns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuannsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
